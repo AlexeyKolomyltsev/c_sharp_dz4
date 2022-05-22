e3866c5c5217b9300eefc4f1eb7c7edd7f672aac
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-22 17:16:57 +0800

    добавил задачу 3

---
+++ 3_even_odd_num_count/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+void PrintArr(int[] array)
+{
+    foreach (int var in array) Console.Write(var + " ");
+}
+
+int[] arr = new int[20];
+int len = arr.Length;
+for (int i = 0; i < len; i++) arr[i] = new Random().Next(100, 1000);
+PrintArr(arr);
+
+int CountEven = 0, CountOdd = 0;  //Инициализация счетчика
+foreach (int var in arr)
+{
+    if (var % 2 == 0) CountEven += 1;
+    else CountOdd += 1;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных элементов массива = {CountEven}, нечетных = {CountOdd}");
