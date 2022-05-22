07a90e28a01d0d461373039ca0c52f2405c1cda6
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-22 18:10:19 +0800

    добавил задачу 5

---
+++ 5_multipl_numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+void PrintArr(int[] array)
+{
+    foreach (int var in array) Console.Write(var + " ");
+}
+
+Console.Write("Задайте длину массива, который будет заполнен случайными числами от -9 до 50 = ");
+int len = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int[len];
+for (int i = 0; i < len; i++) arr[i] = new Random().Next(-9, 51);
+Console.Write("Массив: ");
+PrintArr(arr);
+
+int lenMult = len / 2;           // инициализация массива, заполненного перемножениями пар оригинального массива
+int[] arrMult = new int[lenMult];
+for (int i = 0; i < lenMult; i++)
+{
+    arrMult[i] = arr[i] * arr[^(i + 1)];
+}
+
+Console.WriteLine();
+Console.Write("Массив перемножения пар: ");
+PrintArr(arrMult);
