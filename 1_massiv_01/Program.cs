7677b7858273b4338b3cf1ed61e62c0c0db719f7
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-22 16:34:36 +0800

    добавил задачу 1

---
+++ 1_massiv_01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void PrintArr(int[] array)
+{
+    foreach(int var in array) Console.Write(var + " ");
+}
+
+int[] arr01 = new int[8];
+int len = arr01.Length;
+for (int i = 0; i < len; i++) arr01[i] = new Random().Next(0,2);
+
+PrintArr(arr01);
